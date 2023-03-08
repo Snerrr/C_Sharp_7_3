f7cf32dbf6b2b2cf34355960a965a88076021895
Kornilov Anton <kornilov_antoshka@mail.ru>
2023-03-09 01:13:38 +0300

    DZ4

---
+++ lesson_1/HW1_4/Program.cs
@@ -0,0 +1,9 @@
+﻿//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Напишите одно число");
+int a = int.Parse(Console.ReadLine()!);
+int b = 2;
+while (a>=b)
+{
+    Console.WriteLine(b);
+    b+=2;
+}

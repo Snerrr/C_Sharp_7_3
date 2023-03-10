1c319e23b6f667dd987429fc66152cf93cd643b2
Kornilov Anton <kornilov_antoshka@mail.ru>
2023-03-10 14:40:59 +0300

    21321412

---
+++ lesson_2/1_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// 3. Напишите программу, которая принимает на вход число и проверяет,
+//    кратно ли оно одновременно 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+void Del(int a)
+{
+    Console.WriteLine(a);
+    if (a%7==0 & a%23==0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        Console.WriteLine("Не кратно");
+    }
+    
+}
+int random = new Random().Next(1,1000);
+Del (random);

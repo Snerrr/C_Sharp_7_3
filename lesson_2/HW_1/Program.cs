d433bdb27fc52a709bf1a8165fe161a6f62e9c21
Kornilov Anton <kornilov_antoshka@mail.ru>
2023-03-13 02:25:08 +0300

    2_hw1

---
+++ lesson_2/HW_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int random = new Random().Next(100,1000);
+Console.WriteLine(random);
+void Vtoray(int a)
+{
+     a = a / 10;
+     a = a % 10;
+     Console.WriteLine(a);
+}
+Vtoray(random);

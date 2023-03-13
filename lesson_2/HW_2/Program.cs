1f27d4366fcc6f59572ad64df0850596c9a5630d
Kornilov Anton <kornilov_antoshka@mail.ru>
2023-03-13 03:37:57 +0300

    2_dz_2

---
+++ lesson_2/HW_2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int random = new Random().Next(100,1000000);
+Console.WriteLine(random);
+void troe(int a)
+{
+    if (a<100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        while (a>999)
+        {
+            a = a / 10;
+        }
+        a = a % 10;
+        Console.WriteLine(a);
+    }
+}
+troe(random);

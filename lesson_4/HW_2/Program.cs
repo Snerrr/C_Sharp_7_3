1524b92954f4033a9500b5c36890b4f78defeb41
Snerr <kornilov_antoshka@mail.ru>
2023-03-18 22:20:59 +0300

    4_HW2

---
+++ lesson_4/HW_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int Sum(int a)
+{
+  int summa = 0;
+  while (a >=1)
+  {
+    summa += a % 10;
+    a = a / 10;
+  }
+  return summa;
+}
+Console.WriteLine("Напишите число");
+int a = int.Parse(Console.ReadLine()!);
+int summa = Sum(a);
+Console.WriteLine($"Сумма цифр:{summa}");

c34635d067d5c72d869e86d5a3a8b82413420741
Snerr <kornilov_antoshka@mail.ru>
2023-03-18 17:07:32 +0300

    4_HW1

---
+++ lesson_4/HW_1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+double Step(int a, int b)
+{
+  return Math.Pow(a,b);
+}
+Console.WriteLine("Напишите число и степень в которую его возводить");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+double x = Step(a , b);
+Console.WriteLine(x);

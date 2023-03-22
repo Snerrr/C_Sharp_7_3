d4b0a811c298a130d6b158419dfaccd35ca76b7b
Snerr <kornilov_antoshka@mail.ru>
2023-03-22 19:29:12 +0300

    5_HW2

---
+++ lesson_5/HW_2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] EightMass(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to + 1);
+    return arr;
+}
+
+int Sum (int [] arr)
+{
+  int summa = 0;
+  for (int i = 1; i < arr.Length; i+=2)
+  {
+    summa += arr[i];
+  }
+  return summa;
+}
+int size = int.Parse(Console.ReadLine()!);
+int from = int.Parse(Console.ReadLine()!);
+int to = int.Parse(Console.ReadLine()!);
+int [] arr = EightMass(size, from, to);
+Print(arr);
+Console.WriteLine(Sum(arr));

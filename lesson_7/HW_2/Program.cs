f73f065a597e8f3e2645164f053da96356d8504a
Snerr <kornilov_antoshka@mail.ru>
2023-03-31 11:34:16 +0300

    7_HW

---
+++ lesson_7/HW_2/Program.cs
@@ -0,0 +1,67 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1,7 -> такого числа в массиве нет
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)        
+            Console.Write($" {arr[i, j]} ");        
+        Console.WriteLine();       
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)      
+        for (int j = 0; j < column; j++)        
+            arr[i, j] = new Random().Next(from, to + 1);                
+    
+    return arr;
+}
+
+void Element(int[,] arr, int a, int b)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+    if (a <= row_size && b <= column_size)
+    {
+            a-=1;
+            b-=1;
+            Console.WriteLine(arr[a,b]);
+    }
+    else
+    {
+        Console.WriteLine($"Такого элемента нет: {a},{b}");
+    }
+}
+
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the min number of massive ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the max number of massive ");
+int stop = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Напишите строку");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Напишите столбец");
+int b = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+Print(mass);
+Element(mass,a,b);

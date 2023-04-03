3c108b9559851e5d988b70621e6e31c09efdea8e
Snerr <kornilov_antoshka@mail.ru>
2023-04-03 14:00:33 +0300

    8_HW

---
+++ lesson_8/HW_1/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)        
+            Console.Write($" {arr[i, j]} ");        
+        Console.WriteLine();       
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)      
+        for (int j = 0; j < column; j++)        
+            arr[i, j] = new Random().Next(from, to + 1);                
+    
+    return arr;
+}
+
+int[,] Ymen(int[,] arr)
+{
+  int row_size = arr.GetLength(0);
+  int column_size = arr.GetLength(1);
+  for (int i = 0; i < row_size; i++)
+  {
+    for (int j = 0; j < column_size; j++)
+    {
+      for (int l = 1; l < column_size; l++)
+      {
+        if (arr[i,l-1] < arr[i,l])
+        {
+          int x = arr[i,l-1];
+          arr[i,l-1] = arr[i,l];
+          arr[i,l] = x;
+        }
+      }
+    }
+  }
+  return arr;
+}
+
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the min number of massive ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the max number of massive ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+
+Print(mass);
+Print(Ymen(mass));
---
+++ lesson_8/HW_4/Program.cs
@@ -0,0 +1,87 @@
+﻿// Задайте двумерный массив из целых чисел. Напишите программу,
+// которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random(); // var когда в правой части ясно какой тип данных мы используем
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[] IndexMinVal(int[,] matrix)
+{
+    int min = matrix[0, 0];
+    int indexRow = 0;
+    int indexColumn = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < min)
+            {
+                min = matrix[i, j];
+                indexRow = i;
+                indexColumn = j;
+            }
+
+        }
+    }
+    Console.WriteLine($"{indexRow} -- {indexColumn} === {min}");
+    return new int[] { indexRow, indexRow };
+}
+
+int[,] DeleteMinCrossRowColumn(int[,] matrix, int removeRow, int removeColumn)
+{
+    int[,] newMatrix = new int[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+    int k = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        if (i == removeRow) continue;
+        else
+        {
+            int l = 0;
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (j == removeColumn) continue;
+                else
+                {
+                    newMatrix[k, l] = matrix[i, j];
+                    l++;
+                }
+            }
+            k++;
+        }
+    }
+    return newMatrix;
+}
+
+int row = 5;
+int column = 5;
+
+int[,] array2D = CreateMatrixRndInt(row, column, -9, 9);
+PrintMatrix(array2D);
+int[] ffff = IndexMinVal(array2D);
+
+int[,] dddd = DeleteMinCrossRowColumn(array2D, ffff[0], ffff[1]);
+
+PrintMatrix(dddd);

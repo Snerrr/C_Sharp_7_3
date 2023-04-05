cf933488689c21309dc7bf3548db7bb1b95312b1
Snerr <kornilov_antoshka@mail.ru>
2023-04-05 23:35:20 +0300

    9_HW

---
+++ lesson_9/1_0/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1. Задайте значение N. Напишите программу, которая выведет
+//    все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+
+Console.Write("Enter number N: ");
+int num = int.Parse(Console.ReadLine()!);
+
+void NaturalArr(int N)
+{
+    if(N == 0) return;
+    NaturalArr(N-1);
+    Console.Write($"{N} ");
+}
+NaturalArr(num);
